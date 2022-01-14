73f913128155a9a890d6c0d2bcee42325f760149
Andrio Bobrov <aspyrin@i.ua>
2022-01-14 18:01:57 +0200

    urlpatterns MEDIA_URL

--- paycalc/urls.py
+++ paycalc/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from opt import views
@@ -42,5 +44,5 @@ urlpatterns = [
 #     url(r'^admin/', admin.site.urls),
 # ]
 #
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
