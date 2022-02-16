6c203b6663e911f3b53da5ad2e6da1d87766240a
Andrio Bobrov <aspyrin@i.ua>
2022-02-16 10:46:39 +0200

    в админке реализована функция добавления файлов документов к объявлениям

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from pathlib import Path
+# пользовательские глобальные функции
+from utils.global_func import get_image_name_by_ext
+# модели
 from .models import Branches
 from .models import Notice
 from .models import NoticiesDocs
+
 
 @admin.register(Branches)
 class BranchesAdmin(admin.ModelAdmin):
@@ -9,26 +15,46 @@ class BranchesAdmin(admin.ModelAdmin):
     list_display_links = ('title',)
 
 
+# @admin.register(NoticiesDocs)
+class NoticiesDocsInline(admin.TabularInline):
+    model = NoticiesDocs
+    extra = 0
+    fields = ['document', 'creator', 'published',]
+    readonly_fields = ('document', 'creator', 'published',)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    # def has_delete_permission(self, request, obj=None):
+    #    return False
+
+
 @admin.register(Notice)
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'published', 'creator', 'get_filials_str',)
+    list_display = ('title', 'content', 'published', 'creator', 'get_filials_str', 'get_doc_count',)
     list_display_links = ('title',)
     search_fields = ('title', 'content',)
-    readonly_fields = ('creator',)
+    readonly_fields = ('creator', 'published',)
+    fields = ['title', 'content', 'filial', 'published', 'creator',]
+    inlines = [NoticiesDocsInline,]
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         obj.set_creator(obj.user)
         super(NoticeAdmin, self).save_model(request, obj, form, change)
 
+
 @admin.register(NoticiesDocs)
 class NoticiesDocsAdmin(admin.ModelAdmin):
     list_display = ('notice', 'first_name', 'document', 'published', 'creator', )
-    readonly_fields = ('published', 'creator',)
     list_display_links = ('first_name',)
+    readonly_fields = ('first_name', 'published', 'creator', 'ext_ico', 'ext_name')
+    list_filter = ('notice',)
+    # search_fields = ('notice',)
 
     def save_model(self, request, obj, form, change):
-        obj.user = request.user
-        obj.set_creator(obj.user)
+        obj.set_creator(request.user)
         obj.first_name = obj.document.name
+        obj.ext_name = Path(obj.document.name).suffix[1:].lower()
+        obj.ext_ico = get_image_name_by_ext(obj.ext_name)
         super(NoticiesDocsAdmin, self).save_model(request, obj, form, change)
